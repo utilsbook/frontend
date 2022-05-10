ed480b1b383a98fb87cfb4fabbbb92bb5ca0d2b1
bertonyao <bertonyao@tencent.com>
2022-05-11 01:16:52 +0800

    feat: add icons

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,5 +1,6 @@
 const NotFound = () => {
-  return <>404 Not Found</>;
+  return <>页面开发中，敬请期待～</>;
+  // return <>404 Not Found</>;
 };
 
 export default NotFound;
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,7 +1,28 @@
 import { Menu } from 'antd';
 import './index.scss';
-import { BuildOutlined } from '@ant-design/icons';
+import {
+  AliyunOutlined,
+  AppstoreAddOutlined,
+  BgColorsOutlined,
+  BuildOutlined,
+  CalculatorOutlined,
+  CodeSandboxOutlined,
+} from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
+import {
+  CentosIcon,
+  FigmaIcon,
+  JenkinsIcon,
+  ModaoIcon,
+  MozillaIcon,
+  NginxIcon,
+  NodejsIcon,
+  SASSIcon,
+  SSLIcon,
+  TencentCloudIcon,
+  TypeScriptIcon,
+  VueIcon,
+} from '../../assets/icons';
 
 function getItem(
   label?: any,
@@ -24,9 +45,26 @@ const items = [
     getItem('分割线 Divider', 'divider'),
     getItem('按钮 Button', 'button'),
   ]),
-  // getItem('算法实现', 'algorithms', <CalculatorOutlined />, [
-  //   getItem('Lodash', 'lodash', null, [getItem('isNil', 'isNil')]),
-  // ]),
+  getItem('算法实现', 'algorithms', <CalculatorOutlined />, [
+    getItem('Lodash', 'lodash', null, [getItem('_.isNil（待完成）', 'isNil')]),
+  ]),
+  getItem('其它内容', 'others', <AppstoreAddOutlined />, [
+    getItem('腾讯云（待完成）', 'tengxunyun', <TencentCloudIcon />),
+    getItem('阿里云（待完成）', 'aliyun', <AliyunOutlined />),
+    getItem('CodeSandbox（待完成）', 'codesandbox', <CodeSandboxOutlined />),
+    getItem('PS Online（待完成）', 'ps_onine', <BgColorsOutlined />),
+    getItem('MDN（待完成）', 'mdn', <MozillaIcon />),
+    getItem('墨刀（待完成）', 'modao', <ModaoIcon />),
+    getItem('Figma（待完成）', 'figma', <FigmaIcon />),
+    getItem('Jenkins（待完成）', 'jenkins', <JenkinsIcon />),
+    getItem('CentOS（待完成）', 'centos', <CentosIcon />),
+    getItem('Nginx（待完成）', 'nginx', <NginxIcon />),
+    getItem('Vue（待完成）', 'vue', <VueIcon />),
+    getItem('Nodejs（待完成）', 'nodejs', <NodejsIcon />),
+    getItem('SSL（待完成）', 'ssl', <SSLIcon />),
+    getItem('TypeScript（待完成）', 'typescript', <TypeScriptIcon />),
+    getItem('SASS（待完成）', 'sass', <SASSIcon />),
+  ]),
 ];
 
 const SideBar = () => {
